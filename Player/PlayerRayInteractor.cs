d55422ee92eeb9a21c65d50cc6f3c8fbe9b5effd
rivi <50409554+rivinext@users.noreply.github.com>
2025-11-23 23:52:31 +0900

    Delete Player directory

--- Player/PlayerRayInteractor.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[RequireComponent(typeof(PlayerProximityInteractor))]
-public class PlayerRayInteractor : PlayerProximityInteractor
-{
-    // Deprecated placeholder to keep existing scene references working.
-}
