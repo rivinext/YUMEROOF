57f546735a02d4e4af2c95ee83f288881f348c03
rivi <50409554+rivinext@users.noreply.github.com>
2025-11-30 23:38:10 +0900

    Refactor game clock UI to toggles

--- UI/GameClockUI.cs
+++ UI/GameClockUI.cs
@@ -10,41 +10,43 @@ public class GameClockUI : MonoBehaviour
     public GameClock clock;
     public TMP_Text timeText;
     public TMP_Text dayText;
-    public Button pauseButton;
-    public Button scale1Button;
-    public Button scale2Button;
-    public Button scale4Button;
-    public Button scale6Button;
+    public ToggleGroup timeScaleToggleGroup;
+    public Toggle pauseToggle;
+    public Toggle scale1Toggle;
+    public Toggle scale2Toggle;
+    public Toggle scale4Toggle;
+    public Toggle scale6Toggle;
 
-    private Button _activeButton;
+    private Toggle _activeToggle;
     private GameClock _currentClock;
-    private bool _buttonsInitialized;
-    private bool _timeScaleInitialized;
 
-    void SetActiveButton(Button activeButton)
+    void SetActiveToggle(Toggle activeToggle)
     {
-        Button[] buttons = { pauseButton, scale1Button, scale2Button, scale4Button, scale6Button };
+        Toggle[] toggles = { pauseToggle, scale1Toggle, scale2Toggle, scale4Toggle, scale6Toggle };
 
-        foreach (var button in buttons)
+        foreach (var toggle in toggles)
         {
-            if (button == null)
+            if (toggle == null)
                 continue;
 
-            if (!button.interactable)
-                button.interactable = true;
+            if (!toggle.interactable)
+                toggle.interactable = true;
         }
 
-        _activeButton = activeButton;
+        _activeToggle = activeToggle;
+
+        if (_activeToggle != null && !_activeToggle.isOn)
+            _activeToggle.isOn = true;
 
         var eventSystem = EventSystem.current;
         if (eventSystem != null && ShouldUpdateSelection(eventSystem))
         {
             eventSystem.SetSelectedGameObject(null);
 
-            if (_activeButton != null)
+            if (_activeToggle != null)
             {
-                eventSystem.SetSelectedGameObject(_activeButton.gameObject);
-                _activeButton.Select();
+                eventSystem.SetSelectedGameObject(_activeToggle.gameObject);
+                _activeToggle.Select();
             }
         }
     }
@@ -104,66 +106,64 @@ public class GameClockUI : MonoBehaviour
         _currentClock = clock;
         _currentClock.OnTimeScaleChanged += HandleTimeScaleChanged;
 
-        if (!_buttonsInitialized)
-        {
-            if (pauseButton != null)
-                pauseButton.onClick.AddListener(() =>
-                {
-                    if (_currentClock == null)
-                        return;
-                    _currentClock.SetTimeScale(0f);
-                    SetActiveButton(pauseButton);
-                });
-            if (scale1Button != null)
-                scale1Button.onClick.AddListener(() =>
-                {
-                    if (_currentClock == null)
-                        return;
-                    _currentClock.SetTimeScale(_currentClock.timeScales[0]);
-                    SetActiveButton(scale1Button);
-                });
-            if (scale2Button != null)
-                scale2Button.onClick.AddListener(() =>
-                {
-                    if (_currentClock == null)
-                        return;
-                    _currentClock.SetTimeScale(_currentClock.timeScales[1]);
-                    SetActiveButton(scale2Button);
-                });
-            if (scale4Button != null)
-                scale4Button.onClick.AddListener(() =>
-                {
-                    if (_currentClock == null)
-                        return;
-                    _currentClock.SetTimeScale(_currentClock.timeScales[2]);
-                    SetActiveButton(scale4Button);
-                });
-            if (scale6Button != null)
-                scale6Button.onClick.AddListener(() =>
-                {
-                    if (_currentClock == null)
-                        return;
-                    _currentClock.SetTimeScale(_currentClock.timeScales[3]);
-                    SetActiveButton(scale6Button);
-                });
+        Toggle[] toggles = { pauseToggle, scale1Toggle, scale2Toggle, scale4Toggle, scale6Toggle };
 
-            _buttonsInitialized = true;
+        foreach (var toggle in toggles)
+        {
+            if (toggle == null)
+                continue;
+
+            toggle.onValueChanged.RemoveAllListeners();
+            if (timeScaleToggleGroup != null)
+                toggle.group = timeScaleToggleGroup;
         }
+
+        if (pauseToggle != null)
+            pauseToggle.onValueChanged.AddListener(isOn =>
+            {
+                if (!isOn || _currentClock == null)
+                    return;
+                _currentClock.SetTimeScale(0f);
+                SetActiveToggle(pauseToggle);
+            });
+        if (scale1Toggle != null)
+            scale1Toggle.onValueChanged.AddListener(isOn =>
+            {
+                if (!isOn || _currentClock == null)
+                    return;
+                _currentClock.SetTimeScale(_currentClock.timeScales[0]);
+                SetActiveToggle(scale1Toggle);
+            });
+        if (scale2Toggle != null)
+            scale2Toggle.onValueChanged.AddListener(isOn =>
+            {
+                if (!isOn || _currentClock == null)
+                    return;
+                _currentClock.SetTimeScale(_currentClock.timeScales[1]);
+                SetActiveToggle(scale2Toggle);
+            });
+        if (scale4Toggle != null)
+            scale4Toggle.onValueChanged.AddListener(isOn =>
+            {
+                if (!isOn || _currentClock == null)
+                    return;
+                _currentClock.SetTimeScale(_currentClock.timeScales[2]);
+                SetActiveToggle(scale4Toggle);
+            });
+        if (scale6Toggle != null)
+            scale6Toggle.onValueChanged.AddListener(isOn =>
+            {
+                if (!isOn || _currentClock == null)
+                    return;
+                _currentClock.SetTimeScale(_currentClock.timeScales[3]);
+                SetActiveToggle(scale6Toggle);
+            });
 
         UpdateDayText(_currentClock.currentDay);
         _currentClock.OnDayChanged += UpdateDayText;
 
-        if (!_timeScaleInitialized)
-        {
-            _currentClock.SetTimeScale(_currentClock.timeScales[0]);
-            SetActiveButton(scale1Button);
-            _timeScaleInitialized = true;
-        }
-        else if (_activeButton != null)
-        {
-            SetActiveButton(_activeButton);
-        }
-
+        _currentClock.SetTimeScale(_currentClock.timeScales[0]);
+        SetActiveToggle(scale1Toggle);
         HandleTimeScaleChanged(_currentClock.CurrentScale);
     }
 
@@ -192,35 +192,35 @@ public class GameClockUI : MonoBehaviour
 
     void HandleTimeScaleChanged(float scale)
     {
-        Button targetButton = null;
+        Toggle targetToggle = null;
 
         if (Mathf.Approximately(scale, 0f))
         {
-            targetButton = pauseButton;
+            targetToggle = pauseToggle;
         }
         else if (_currentClock != null)
         {
             var scales = _currentClock.timeScales;
             if (scales != null && scales.Length > 0)
             {
-                Button[] scaleButtons = { scale1Button, scale2Button, scale4Button, scale6Button };
-                int maxIndex = Mathf.Min(scales.Length, scaleButtons.Length);
+                Toggle[] scaleToggles = { scale1Toggle, scale2Toggle, scale4Toggle, scale6Toggle };
+                int maxIndex = Mathf.Min(scales.Length, scaleToggles.Length);
 
                 for (int i = 0; i < maxIndex; i++)
                 {
                     if (Mathf.Approximately(scale, scales[i]))
                     {
-                        targetButton = scaleButtons[i];
+                        targetToggle = scaleToggles[i];
                         break;
                     }
                 }
 
-                if (targetButton == null && scaleButtons[0] != null && Mathf.Approximately(scale, 1f))
-                    targetButton = scaleButtons[0];
+                if (targetToggle == null && scaleToggles[0] != null && Mathf.Approximately(scale, 1f))
+                    targetToggle = scaleToggles[0];
             }
         }
 
-        if (targetButton != null)
-            SetActiveButton(targetButton);
+        if (targetToggle != null)
+            SetActiveToggle(targetToggle);
     }
 }
